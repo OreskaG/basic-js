9b4cabd7774515d798851760baee64756a41b87f
OreskaG <Ctepanrfo@gmail.com>
2022-04-29 13:43:28 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if ( sampleActivity != Number(sampleActivity) ) { return false }
+  if ( typeof(sampleActivity) !== "string" ) { return false }
+  if ( sampleActivity == 0 ) { return false }
+  if ( Number(sampleActivity) < 0 ) { return false }
+  if ( Number(sampleActivity) >= MODERN_ACTIVITY ) { return false }
+  let age = 0 ;
+  let k = 0.693 / HALF_LIFE_PERIOD ;
+  
+  age = Math.log( MODERN_ACTIVITY / sampleActivity ) / k ;
+
+  return Math.ceil(age) ;
 }
 
 module.exports = {
