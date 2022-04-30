94e2c3d301f45536f8b8cadc8a1df096f276d84d
OreskaG <Ctepanrfo@gmail.com>
2022-04-30 13:21:22 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type) { this.type = type } ;
+
+  encrypt(message, key) {
+    if ( !message ) { throw new Error('Incorrect arguments!') }
+    if ( !key ) { throw new Error('Incorrect arguments!') }
+    message = message.toUpperCase() ;
+    key = key.toUpperCase() ;
+    let result = '';
+    key = key.padEnd( message.length, key ) ;
+
+      for ( let i = 0 , j = 0 ; i < message.length ; i++) {
+        if ( message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90 ) {
+          let char = String.fromCharCode( 65 + (message.charCodeAt(i) + key.charCodeAt(j) - 2 * 65 ) % 26 ) ;
+          console.log(char)
+          result = result + char ;
+          j++ ;
+          continue ;
+        } 
+        result = result + message[i] ; 
+      }
+    if ( this.type == false ) { result = result.split('').reverse().join('') } ;
+    return result ;
+} ;
+
+  decrypt(message, key) {
+    if ( !message ) { throw new Error('Incorrect arguments!') }
+    if ( !key ) { throw new Error('Incorrect arguments!') }
+    message = message.toUpperCase() ;
+    key = key.toUpperCase() ;
+    let result = '';
+    key = key.padEnd( message.length, key ) ;
+
+      for ( let i = 0 , j = 0 ; i < message.length ; i++) {
+        if ( message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90 ) {
+          let char = String.fromCharCode( 65 + (message.charCodeAt(i) - key.charCodeAt(j) + 26 ) % 26 ) ;
+          console.log(char)
+          result = result + char ;
+          j++ ;
+          continue ;
+        } 
+        result = result + message[i] ; 
+      }
+    if ( this.type == false ) { result = result.split('').reverse().join('') } ;
+    return result ;
   }
 }
 
