b6270551147919cda7be632629d9011823472007
OreskaG <Ctepanrfo@gmail.com>
2022-04-30 18:21:56 +0300

    feat: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {} ;
+  if ( domains.length === 0 ) return result ;
+  let arr = [] ;
+  let arr1 = [] ;
+  for ( let i = 0 ; i < domains.length ; i++ ) {
+    let x = domains[i].split('.') ;
+    for ( let j = 0 ; j < x.length ; j++ ) {
+      arr1.push('.' + x[j])
+    }
+    arr1.reverse() ;
+    arr.push(arr1) ;
+    arr1 = [] ;
+  }
+  arr.forEach(arr => arr.reduce((item1, item2) => {
+      if (result[item1 + item2]) {
+        result[item1 + item2] = result[item1 + item2] + 1 ;
+        return item1 + item2 ;
+      }
+      result[item1 + item2] = 1 ;
+      return item1 + item2 ;
+  }, ""))
+  return result ;
 }
-
 module.exports = {
   getDNSStats
 };
